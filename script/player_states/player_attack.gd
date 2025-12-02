2e9907e4b73eaf0dec9b83b55e432bb274db7bcd
ElCono <murphymx100@gmail.com>
2025-12-01 21:04:29 -0300

    nose

--- script/player_states/player_attack.gd
+++ script/player_states/player_attack.gd
@@ -1,10 +1,16 @@
 extends Player_gravity
 
+func on_physic_process(delta):
+	super(delta)
+	var dir = Input.get_axis("left", "right")
+	handle_acceleration(dir,speed, delta)
+	apply_friction(dir, delta)
+
 func start():
 	if not player.attack_egg:
 		state_machine.change_to("idle")
 	else:
-		player.anim.play("attack")
+		player.anim.play("attack", -1, 2.0)
 
 
 func _on_anim_animation_finished(anim_name: StringName) -> void:
--- script/player_states/player_shoot.gd
+++ script/player_states/player_shoot.gd
@@ -1,5 +1,10 @@
 extends Player_gravity
 
+func on_physic_process(delta):
+	super(delta)
+	var dir = Input.get_axis("left", "right")
+	handle_acceleration(dir,speed, delta)
+	apply_friction(dir, delta)
 
 func start():
 	if not player.shoot_egg:
