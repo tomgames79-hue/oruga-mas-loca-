dd577324f0a466a45124f3205beae4ba7965d246
ElCono <murphymx100@gmail.com>
2025-12-24 07:27:07 -0300

    1.3.2
    menu de pausa arreglado

--- escenas/levels/pausa.gd
+++ escenas/levels/pausa.gd
@@ -1,8 +1,6 @@
 extends Node
 
-@onready var opciones: Panel = $Opciones
-
-
+@onready var opciones: Panel = $".."
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pausa"):
@@ -17,3 +15,7 @@ func _ready() -> void:
 func _on_back_pressed() -> void:
 	opciones.visible = false
 	get_tree().paused = false
+
+func _process(_delta: float) -> void:
+	if !get_tree().paused:
+		opciones.visible = false
